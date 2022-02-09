80c3f32f56fac882c6b6d6aa98767d3a99985083
sgb <sgb2351863806@gmail.com>
2022-02-09 17:54:42 +0800

    提交git成功

--- src/main/java/com/atguigu/add.java
+++ src/main/java/com/atguigu/add.java
@@ -3,14 +3,11 @@ package com.atguigu;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class add {
     private Integer id;
     private String name;
-
+    private Boolean flag;
 }
